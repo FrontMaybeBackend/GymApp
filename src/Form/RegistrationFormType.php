20e59072f29f8cc220a1c3439343e3d6e1f4ace1
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-06-30 00:22:39 +0200

    maked validation for register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -25,15 +27,35 @@ class RegistrationFormType extends AbstractType
                         'placeholder'=>'Username',
                         'class'=>'form-control',
                     ],
+                    'constraints'=>[
+                        new NotBlank([
+                            'message'=>'Please enter a username',
+                        ]),
+                    ]
                 ]
             )
+            ->add('email',EmailType::class,[
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder'=>'Email',
+                    'class'=>'form-control',
+                ],
+                'constraints'=>[
+                    new NotBlank([
+                        'message'=>'Please enter a email',
+                    ]),
+                    new Email([
+                        'message' => 'Bad format email.',
+                    ])
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
-                ],
+                                   ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -41,7 +63,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password',
                     'placeholder'=>'Password',
-                    'class'=>'form-control ',],
+                    'class'=>'form-control'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',

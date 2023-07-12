16891d34e2c540993f5d1d2c8f8462450264ba0d
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-07-12 13:08:09 +0200

    I added a user profile editing controller and wdok to that but haven't finished yet.

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserProfileForm;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -24,6 +27,36 @@ class ProfileController extends AbstractController
 
         return $this->render('profile/index.html.twig', [
             'profile' => $profile,
+        ]);
+    }
+
+    #[Route('/profile/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserRepository $userRepository, User $user, EntityManagerInterface $entityManager): Response
+    {
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserProfileForm::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form ->isValid()){
+            $data = $form->getData();
+
+            $user->setUsername($data['username']);
+            $user->setPassword(password_hash($data['password'],PASSWORD_DEFAULT));
+            $user->setEmail($data['email']);
+
+            //dodac opis do bazy danych, numer treningow i rodzaj
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit_profile.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

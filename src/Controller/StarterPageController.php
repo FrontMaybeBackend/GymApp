c90e231f3385efedbf7c57c1d8ed2904911c7860
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-08-02 13:48:24 +0200

    added another cards with statistics

--- src/Controller/StarterPageController.php
+++ src/Controller/StarterPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\TrainingRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class StarterPageController extends AbstractController
 {
     #[Route('/starter/page', name: 'app_starter_page')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, TrainingRepository $trainingRepository,): Response
     {
+            //Wyswietlenie liczby userow
+        $users =$userRepository->findAll();
+        $numberOfUsers = count($users);
+
+        $trainings = $trainingRepository->findAll();
+        $numberOfTrainings = count($trainings);
+
+
         return $this->render('starter_page/index.html.twig', [
             'controller_name' => 'StarterPageController',
+            'numberOfUsers'=>$numberOfUsers,
+            'users'=> $users,
+            'trainings'=>$trainingRepository,
+            'numberOfTrainings'=>$numberOfTrainings,
         ]);
+
+
     }
+
 }

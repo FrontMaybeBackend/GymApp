377114ca4c6269929407cf25e3ffddf0b24afd3c
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-08-07 20:22:00 +0200

    added add controller,routes,template

--- src/Controller/FriendsController.php
+++ src/Controller/FriendsController.php
@@ -20,8 +20,13 @@ class FriendsController extends AbstractController
         ]);
     }
 
+    #[Route('/friends_add', name:'app_add_friends')]
     public function add(FriendsRepository $friendsRepository, UserRepository $userRepository): Response
     {
+        return $this->render('friends/index.html.twig', [
+            'friends'=>$friendsRepository->findAll(),
+
+        ]);
 
     }
 }

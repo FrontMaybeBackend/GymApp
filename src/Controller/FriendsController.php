f282162af5b336639466260bbb19821da256231b
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-08-07 20:15:54 +0200

    Added controller for entity friends:

---
+++ src/Controller/FriendsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FriendsRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FriendsController extends AbstractController
+{
+    #[Route('/friends', name: 'app_friends')]
+    public function index(FriendsRepository $friendsRepository,): Response
+    {
+
+        return $this->render('friends/index.html.twig', [
+            'friends'=>$friendsRepository->findAll(),
+
+        ]);
+    }
+
+    public function add(FriendsRepository $friendsRepository, UserRepository $userRepository): Response
+    {
+
+    }
+}

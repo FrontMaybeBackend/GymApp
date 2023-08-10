28011a4865dee3d1132ec47b59ed260c77e58a84
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-08-10 11:49:11 +0200

    now user can added friend

--- src/Controller/FriendsController.php
+++ src/Controller/FriendsController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\FriendsRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,15 +25,18 @@ class FriendsController extends AbstractController
         ]);
     }
 
-    #[Route('/friends/add', name:'app_add_friends', methods: ['POST'])]
-    public function add(Request $request, FriendsRepository $friendsRepository, UserRepository $userRepository,User $user, EntityManager $entityManager): Response
+    #[Route('/friends/add/{id}', name:'app_add_friends', methods: ['POST'])]
+    public function add(Request $request, FriendsRepository $friendsRepository, UserRepository $userRepository, EntityManagerInterface $entityManager, $id): Response
     {
-        $friends = new Friends();
-        $add = $friends->addUser($user);
 
-        if($request->getMethod()){
-            $whut = $this->getUser();
-            $entityManager->persist($whut);
+
+
+        if($request->isMethod('POST')){
+            $user = $userRepository->find($id);
+            $friends = new Friends();
+            $friends->addUser($user);
+
+            $entityManager->persist($friends);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_friends');

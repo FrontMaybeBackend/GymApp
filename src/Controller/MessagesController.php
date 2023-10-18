4668dc3c7a9ccd7fb7d21d7f74c4d07ef74cfe57
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-10-18 20:32:55 +0200

    added method with send messages to db

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Messages;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MessagesController extends AbstractController
 {
-    #[Route('/messages', name: 'app_messages')]
-    public function index(): Response
+    #[Route('/messages/', name: 'app_messages_send', methods: 'POST')]
+    public function add(Request $request, EntityManagerInterface $entityManager, UserInterface $user): Response
     {
-        return $this->render('messages/index.html.twig', [
-            'controller_name' => 'MessagesController',
-        ]);
+
+        if ($request->isMethod('POST')) {
+            $user->getUserIdentifier();
+            $username = $user->getUsername();
+
+            $message = new Messages();
+            $message->setFromUser($username);
+            $message->setToUser($_POST['toUser']);
+            $message->setTitle($_POST['title']);
+            $message->setDescription($_POST['description']);
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            flash()->addSuccess('Message already send');
+
+        }
+
+        return $this->render('starter_page/index.html.twig');
     }
 }

aebae71c581bea5b773c2752873c3f41e18ef674
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-10-18 19:02:13 +0200

    add styles, make new entity Messages

---
+++ src/Entity/Messages.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessagesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessagesRepository::class)]
+class Messages
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fromUser = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $toUser = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFromUser(): ?string
+    {
+        return $this->fromUser;
+    }
+
+    public function setFromUser(string $fromUser): static
+    {
+        $this->fromUser = $fromUser;
+
+        return $this;
+    }
+
+    public function getToUser(): ?string
+    {
+        return $this->toUser;
+    }
+
+    public function setToUser(string $toUser): static
+    {
+        $this->toUser = $toUser;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}

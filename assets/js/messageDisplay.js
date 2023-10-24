845fadde116e1e28dbe02dede85b0c41d71066ac
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-10-24 21:43:53 +0200

    change js file, now i can send message to the same person who write to me

--- assets/js/messageDisplay.js
+++ assets/js/messageDisplay.js
@@ -1,28 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let messageDivs = document.querySelectorAll('.message'); // Znajdź wszystkie elementy o klasie "message"
-    let testDiv = document.querySelector('#test'); // Znajdź div o id "test"
-    console.log('Skrypt został załadowany.');
+    let messageDivs = document.querySelectorAll('.message');
+    let testDiv = document.querySelector('#test');
+    let toUserInput = document.querySelector('input[name="toUser"]');
+    let titleInput = document.querySelector('input[name="title"]');
 
     messageDivs.forEach(function (messageDiv) {
         messageDiv.addEventListener('click', function () {
             let userData = JSON.parse(messageDiv.getAttribute('data-user'));
 
-            // Znajdź istniejące elementy h6, h5,p,span i h4 w divie "test"
-            let testDiv = document.getElementById('test');
+            toUserInput.value = userData.fromUser;
+            titleInput.value = userData.title;
+
             let h6 = testDiv.querySelector('h6');
             let h5 = testDiv.querySelector('h5');
             let textarea = testDiv.querySelector('textarea');
 
-
-
-            h6.textContent =  "Username: "  + userData.fromUser;
+            h6.textContent = "Username: " + userData.fromUser;
             h5.textContent = "Title: " + userData.title;
-            textarea.textContent = "Description: " +  userData.description;
-
-            let usernameLabel = testDiv.querySelector('label[for="toUser"]');
-            let titleLabel = testDiv.querySelector('label[for="title"]');
-            usernameLabel.textContent = "Username: " + userData.fromUser;
-            titleLabel.textContent = "Title: " + userData.title;
+            textarea.textContent = "Description: " + userData.description;
         });
     });
 });
+
